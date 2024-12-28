63dff1219974c3b3dcacdfb51beecd1a2da09a82
bagelswitch <5595627+bagelswitch@users.noreply.github.com>
2024-12-27 16:10:20 -0800

    Add CPU load graphical display on secondary monitor

--- BagelAura/BagelAura.cs
+++ BagelAura/BagelAura.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using AuraServiceLib;
 using Microsoft.Win32;
 
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+
 namespace BagelAura
 {
     internal class BagelAura
@@ -21,6 +25,22 @@ namespace BagelAura
         static List<IAuraRgbLight> stickOneLights = null;
         static List<IAuraRgbLight> stickTwoLights = null;
         static List<IAuraRgbLight> mBoardLights = null;
+
+        private static System.Timers.Timer aTimer;
+
+        static PerformanceCounter cpu = new PerformanceCounter
+        {
+            CategoryName = "Processor Information",
+            CounterName = "% Processor Utility",
+            InstanceName = "_Total"
+        };
+
+        static SimpleMovingAverage calculator = new SimpleMovingAverage(k: 20);
+
+        // attempt to draw rectangles directly to screen
+        static CPUDisplay cpud = new CPUDisplay();
+
+        static int k = 1;
 
         static void ObtainControl(Boolean reEnum = true)
         {
@@ -113,6 +133,16 @@ namespace BagelAura
             }
         }
 
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(60);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
         static void Main(string[] args)
         {
             Process process = Process.GetCurrentProcess();
@@ -120,95 +150,99 @@ namespace BagelAura
             process.Exited += new EventHandler(OnExit);
             SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(OnPowerModeChanged);
 
-            var cpu = new PerformanceCounter
-            {
-                CategoryName = "Processor Information",
-                CounterName = "% Processor Utility",
-                InstanceName = "_Total"
-            };
+            SetTimer();
 
-            var calculator = new SimpleMovingAverage(k: 20);
+            Application.Run();
 
-            int k = 1;
-            while (true)
+            aTimer.Stop();
+            aTimer.Dispose();
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (active)
             {
-                if (active)
+                int cpuLoad = calculator.Update((int)(cpu.NextValue() * 100) - 500);
+
+                if (k == 1)
                 {
-                    int cpuLoad = calculator.Update((int)(cpu.NextValue() * 100) - 500);
-
-                    if (k == 1)
-                    {
-                        ObtainControl();
-                    } else if (k >= 5000)
-                    {
-                        ObtainControl(false);
-                        k = 1;
-                    }
-
-                    // Set LEDs on mboard i/o panel
-                    int blue = 0;
-                    int green = 0;
-                    int red = 0;
-                    if (cpuLoad > 6000)
-                    {
-                        float intensity = (float)(cpuLoad - 6000) / (float)1500;
-                        if (intensity > 1.0) intensity = (float)1.0;
-                        if (intensity < 0.0) intensity = (float)0.0;
-                        blue = 0;
-                        red = 255;
-                        green = 255 - (int)(255 * intensity);
-                    } else if (cpuLoad > 4500)
-                    {
-                        float intensity = (float)(cpuLoad - 4500) / (float)1500;
-                        if (intensity > 1.0) intensity = (float)1.0;
-                        if (intensity < 0.0) intensity = (float)0.0;
-                        blue = 0;
-                        red = (int)(255 * intensity);
-                        green = 255;
-                    }
-                    else if (cpuLoad > 3000)
-                    {
-                        float intensity = (float)(cpuLoad - 3000) / (float)1500;
-                        if (intensity > 1.0) intensity = (float)1.0;
-                        if (intensity < 0.0) intensity = (float)0.0;
-                        red = 0;
-                        blue = 255 - (int)(255 * intensity);
-                        green = 255;
-                    } else if (cpuLoad > 1500)
-                    {
-                        float intensity = (float)(cpuLoad - 1500) / (float)1500;
-                        if (intensity > 1.0) intensity = (float)1.0;
-                        if (intensity < 0.0) intensity = (float)0.0;
-                        red = 0;
-                        green = (int)(255 * intensity);
-                        blue = 255;
-                    } else
-                    {
-                        float intensity = (float)(cpuLoad) / (float)1500;
-                        if (intensity > 1.0) intensity = (float)1.0;
-                        if (intensity < 0.0) intensity = (float)0.0;
-                        red = 0;
-                        green = 0;
-                        blue = (int)(255 * intensity);
-                    }
-                    uint iocolor = ColorFromBytes((byte) blue, (byte) green, (byte) red);
-                    mBoardLights[0].Color = iocolor;
-                    mBoardLights[1].Color = iocolor;
-                    mBoardLights[2].Color = iocolor;
-                    mBoard.Apply();
-
-                    // Traverse all LEDs on DRAM sticks one and two
-                    for (int i = 0; i < 8; i++)
-                    {
-                        stickOneLights[i].Color = iocolor;
-                        stickTwoLights[i].Color = iocolor;
-                    }
-                    stickOne.Apply();
-                    stickTwo.Apply();
+                    ObtainControl();
                 }
-                System.Threading.Thread.Sleep(60);
-                k++;
+                else if (k >= 5000)
+                {
+                    ObtainControl(false);
+                    k = 1;
+                }
+
+                // Set LEDs on mboard i/o panel
+                int blue = 0;
+                int green = 0;
+                int red = 0;
+                if (cpuLoad > 6000)
+                {
+                    float intensity = (float)(cpuLoad - 6000) / (float)1500;
+                    if (intensity > 1.0) intensity = (float)1.0;
+                    if (intensity < 0.0) intensity = (float)0.0;
+                    blue = 0;
+                    red = 255;
+                    green = 255 - (int)(255 * intensity);
+                }
+                else if (cpuLoad > 4500)
+                {
+                    float intensity = (float)(cpuLoad - 4500) / (float)1500;
+                    if (intensity > 1.0) intensity = (float)1.0;
+                    if (intensity < 0.0) intensity = (float)0.0;
+                    blue = 0;
+                    red = (int)(255 * intensity);
+                    green = 255;
+                }
+                else if (cpuLoad > 3000)
+                {
+                    float intensity = (float)(cpuLoad - 3000) / (float)1500;
+                    if (intensity > 1.0) intensity = (float)1.0;
+                    if (intensity < 0.0) intensity = (float)0.0;
+                    red = 0;
+                    blue = 255 - (int)(255 * intensity);
+                    green = 255;
+                }
+                else if (cpuLoad > 1500)
+                {
+                    float intensity = (float)(cpuLoad - 1500) / (float)1500;
+                    if (intensity > 1.0) intensity = (float)1.0;
+                    if (intensity < 0.0) intensity = (float)0.0;
+                    red = 0;
+                    green = (int)(255 * intensity);
+                    blue = 255;
+                }
+                else
+                {
+                    float intensity = (float)(cpuLoad) / (float)1500;
+                    if (intensity > 1.0) intensity = (float)1.0;
+                    if (intensity < 0.0) intensity = (float)0.0;
+                    red = 0;
+                    green = 0;
+                    blue = (int)(255 * intensity);
+                }
+                uint iocolor = ColorFromBytes((byte)blue, (byte)green, (byte)red);
+                mBoardLights[0].Color = iocolor;
+                mBoardLights[1].Color = iocolor;
+                mBoardLights[2].Color = iocolor;
+                mBoard.Apply();
+
+                // Traverse all LEDs on DRAM sticks one and two
+                for (int i = 0; i < 8; i++)
+                {
+                    stickOneLights[i].Color = iocolor;
+                    stickTwoLights[i].Color = iocolor;
+                }
+                stickOne.Apply();
+                stickTwo.Apply();
+
+                Color activecolor = Color.FromArgb(red, green, blue);
+                cpud.activecolor = activecolor;
+                cpud.Invalidate();
             }
+            k++;
         }
     }
 }
---
+++ BagelAura/CPUDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BagelAura
+{
+    public partial class CPUDisplay : Form
+    {
+        public Color activecolor = Color.Black;
+
+        public CPUDisplay()
+        {
+            InitializeComponent();
+
+            this.Bounds = new Rectangle(0, 0, 1100, 100);
+            this.TopMost = true;
+            Point TopLeft = Screen.AllScreens[0].WorkingArea.Location;
+            TopLeft.Y = TopLeft.Y + 2400;
+            this.Location = TopLeft;
+            this.BackColor = Color.Black;
+            this.TransparencyKey = Color.Black;
+            this.Visible = true;
+        }
+
+        private void CPUDisplay_Paint(object sender, PaintEventArgs e)
+        {
+            using (var gfx = e.Graphics) {
+                using (var brush = new SolidBrush(this.activecolor))
+                {
+                    gfx.FillRectangle(brush, 0, 0, 1100, 100);
+                }
+            }
+        }
+    }
+}
